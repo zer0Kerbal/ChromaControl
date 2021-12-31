58d3aaba005d85ff2368da3b3948234dbc60fe13
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-12-30 23:33:17 -0600

    Create SceneManager.cs

---
+++ source/SceneManagers/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿namespace KspChromaControl.SceneManagers
+{
+    using KspChromaControl.ColorSchemes;
+
+    /// <summary>
+    ///     Allows creation of keyboard color managers for multiple scenes.
+    /// </summary>
+    internal interface ISceneManager
+    {
+        /// <summary>
+        ///     Gets the keyboard color scheme for the current frame.
+        /// </summary>
+        /// <returns>The new color scheme.</returns>
+        ColorScheme GetScheme();
+    }
+}
