614fffea2030e2d05c5a4a00c280900f1492794e
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-12-30 22:59:43 -0600

    Create LogoAnimation.cs

---
+++ source/Animations/LogoAnimation.cs
@@ -0,0 +1,43 @@
+﻿namespace KspChromaControl.Animations
+{
+    using System.Collections.Generic;
+    using KspChromaControl.ColorSchemes;
+    using UnityEngine;
+
+    /// <summary>
+    ///     Slowly displays the KSP logo from a blue base scheme.
+    /// </summary>
+    internal class LogoAnimation : KeyboardAnimation
+    {
+        /// <summary>
+        ///     The frames this animation consists of.
+        /// </summary>
+        private static readonly ColorScheme[] frames;
+
+        /// <summary>
+        ///     List of scenes this animation is valid in.
+        /// </summary>
+        private static readonly List<GameScenes> validScenes = new List<GameScenes>
+        {
+            GameScenes.MAINMENU,
+            GameScenes.SPACECENTER,
+            GameScenes.TRACKSTATION,
+            GameScenes.CREDITS
+        };
+
+        /// <summary>
+        ///     Static constructor interpolates from blue to logo.
+        /// </summary>
+        static LogoAnimation()
+        {
+            frames = AnimationUtils.InterpolateFrames(new ColorScheme(Color.blue), new LogoScheme(), 20);
+        }
+
+        /// <summary>
+        ///     Initializes the base keyboard animation object.
+        /// </summary>
+        public LogoAnimation() : base(10, validScenes, frames)
+        {
+        }
+    }
+}
