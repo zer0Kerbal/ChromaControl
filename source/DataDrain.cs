c335fe29f7fedccc33d8c6f566f30aaa0771f03a
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-12-30 23:00:17 -0600

    Create DataDrain.cs

---
+++ source/DataDrain.cs
@@ -0,0 +1,16 @@
+﻿namespace KspChromaControl
+{
+    using KspChromaControl.ColorSchemes;
+
+    /// <summary>
+    ///     Implement this to use the mod with other devices.
+    /// </summary>
+    internal interface IDataDrain
+    {
+        /// <summary>
+        ///     Sends the requested color scheme to the implemented output channel
+        /// </summary>
+        /// <param name="scheme">The color scheme to apply to the implemented output device</param>
+        void Send(ColorScheme scheme);
+    }
+}
