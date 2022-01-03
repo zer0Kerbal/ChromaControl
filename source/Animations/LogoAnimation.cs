80817ffcaccf2857ea53afcdbdf7dc0505e278f8
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-01-02 18:59:21 -0600

    Update LogoAnimation.cs
    # Code
    - Lint comments

--- source/Animations/LogoAnimation.cs
+++ source/Animations/LogoAnimation.cs
@@ -4,19 +4,13 @@
     using KspChromaControl.ColorSchemes;
     using UnityEngine;
 
-    /// <summary>
-    ///     Slowly displays the KSP logo from a blue base scheme.
-    /// </summary>
+    /// <summary>Slowly displays the KSP logo from a blue base scheme.</summary>
     internal class LogoAnimation : KeyboardAnimation
     {
-        /// <summary>
-        ///     The frames this animation consists of.
-        /// </summary>
+        /// <summary>The frames this animation consists of.</summary>
         private static readonly ColorScheme[] frames;
 
-        /// <summary>
-        ///     List of scenes this animation is valid in.
-        /// </summary>
+        /// <summary>List of scenes this animation is valid in.</summary>
         private static readonly List<GameScenes> validScenes = new List<GameScenes>
         {
             GameScenes.MAINMENU,
@@ -25,17 +19,13 @@
             GameScenes.CREDITS
         };
 
-        /// <summary>
-        ///     Static constructor interpolates from blue to logo.
-        /// </summary>
+        /// <summary>Static constructor interpolates from blue to logo.</summary>
         static LogoAnimation()
         {
             frames = AnimationUtils.InterpolateFrames(new ColorScheme(Color.blue), new LogoScheme(), 20);
         }
 
-        /// <summary>
-        ///     Initializes the base keyboard animation object.
-        /// </summary>
+        /// <summary>Initializes the base keyboard animation object.</summary>
         public LogoAnimation() : base(10, validScenes, frames)
         {
         }
