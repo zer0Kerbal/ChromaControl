9d162b1087cca0626e5b9301a08155dee2cfb7e3
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-01-02 23:31:36 -0600

    Update LogoScheme.cs
    # Code
    - Lint comments

--- source/ColorSchemes/LogoScheme.cs
+++ source/ColorSchemes/LogoScheme.cs
@@ -2,15 +2,10 @@
 {
     using UnityEngine;
 
-    /// <summary>
-    ///     Displays an image on the keyboard vaguely similar to the logo minus the
-    ///     KSP text.
-    /// </summary>
+    /// <summary>Displays an image on the keyboard vaguely similar to the logo minus the KSP text.</summary>
     internal class LogoScheme : ColorScheme
     {
-        /// <summary>
-        ///     Overlays the defined keys on top of a blue base color scheme.
-        /// </summary>
+        /// <summary>Overlays the defined keys on top of a blue base color scheme.</summary>
         public LogoScheme() : base(Color.blue)
         {
             KeyCode[] redkeys =
