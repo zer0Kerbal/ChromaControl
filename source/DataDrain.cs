c6fffe1bd7b8bd5a649853b7acaf95390d79690e
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-01-02 19:20:44 -0600

    Update DataDrain.cs
    # Code
    - Lint comments

--- source/DataDrain.cs
+++ source/DataDrain.cs
@@ -2,14 +2,10 @@
 {
     using KspChromaControl.ColorSchemes;
 
-    /// <summary>
-    ///     Implement this to use the mod with other devices.
-    /// </summary>
+    /// <summary>Implement this to use the mod with other devices.</summary>
     internal interface IDataDrain
     {
-        /// <summary>
-        ///     Sends the requested color scheme to the implemented output channel
-        /// </summary>
+        /// <summary>Sends the requested color scheme to the implemented output channel</summary>
         /// <param name="scheme">The color scheme to apply to the implemented output device</param>
         void Send(ColorScheme scheme);
     }
