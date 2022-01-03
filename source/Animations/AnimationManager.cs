6df39f2d22056c97a8070a14fc06724f43bbd9c5
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-01-02 19:00:53 -0600

    Update AnimationManager.cs
    # Code
    - Lint comments

--- source/Animations/AnimationManager.cs
+++ source/Animations/AnimationManager.cs
@@ -2,31 +2,21 @@
 {
     using KspChromaControl.ColorSchemes;
 
-    /// <summary>
-    ///     Handles all animations to avoid confusion and multiple animations running at the same time.
-    /// </summary>
+    /// <summary>Handles all animations to avoid confusion and multiple animations running at the same time.</summary>
     internal class AnimationManager
     {
-        /// <summary>
-        ///     Singleton instance
-        /// </summary>
+        /// <summary>Singleton instance</summary>
         private static AnimationManager instance;
 
-        /// <summary>
-        ///     The currently running animation or null
-        /// </summary>
+        /// <summary>The currently running animation or null</summary>
         private KeyboardAnimation activeAnimation;
 
-        /// <summary>
-        ///     Private constructor to avoid out of singleton instantiation
-        /// </summary>
+        /// <summary>Private constructor to avoid out of singleton instantiation</summary>
         private AnimationManager()
         {
         }
 
-        /// <summary>
-        ///     Instance getter
-        /// </summary>
+        /// <summary>Instance getter</summary>
         public static AnimationManager Instance
         {
             get
@@ -35,26 +25,19 @@
                 {
                     instance = new AnimationManager();
                 }
-
                 return instance;
             }
         }
 
-        /// <summary>
-        ///     Set the current animation
-        /// </summary>
+        /// <summary>Set the current animation</summary>
         /// <param name="animation">the animation to display</param>
         public void SetAnimation(KeyboardAnimation animation) => this.activeAnimation = animation;
 
-        /// <summary>
-        ///     Fetches one frame from the animation
-        /// </summary>
+        /// <summary>Fetches one frame from the animation</summary>
         /// <returns>the current animation frame</returns>
         public ColorScheme GetFrame() => this.AnimationRunning() ? this.activeAnimation.GetFrame() : new ColorScheme();
 
-        /// <summary>
-        ///     Checks if there is still an animation running.
-        /// </summary>
+        /// <summary>Checks if there is still an animation running.</summary>
         /// <returns></returns>
         public bool AnimationRunning() => this.activeAnimation != null && !this.activeAnimation.IsFinished();
     }
