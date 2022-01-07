77f326d62c5ab3ac5605c66faa7894e89607ca8e
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-01-07 02:03:59 -0600

    Update KSPChromaControl.cs
    - Add GPLv3 notice to code
    - change from
      -  [KSPAddon(KSPAddon.Startup.Instantly, false)]
      -  [KSPAddon(KSPAddon.Startup.EveryScene, false)]

--- source/KSPChromaControl.cs
+++ source/KSPChromaControl.cs
@@ -1,5 +1,26 @@
-﻿using System;
+﻿/*
+ * KspChromaControl
+ * Copyright(C) Copyright © 2016, 2021, 2022 zer0Kerbal
+ * zer0Kerbal at hotmail dot com
+ * KGEx
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Linq;
+using Corale.Colore;
 
 [assembly: CLSCompliant(false)]
 
@@ -12,7 +33,8 @@ namespace KspChromaControl
     using UnityEngine;
 
     /// <summary>The main class, managing the keyboard appearance for every kind of scene KSPuses.</summary>
-    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    //[KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
     public class KspChromaPlugin : MonoBehaviour
     {
         private readonly List<IDataDrain> dataDrains = new List<IDataDrain>();
